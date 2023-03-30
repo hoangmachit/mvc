4e86cd97ea4a38ed757a57b6cf7d6f62fa869b9a
AGL_Mạch Văn Hoàng (ホアン) <81142674+machvanhoang@users.noreply.github.com>
2023-03-30 17:19:24 +0700

    SEO

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -11,11 +11,17 @@ import Newletter from "@/components/Newletter";
 import Partner from "@/components/Partner";
 import Categories from "@/components/Categories";
 import SliderAds from "@/components/SliderAds";
+import SEO from "@/components/Seo";
 export default async function Home() {
   const data = await getDataIndex();
-  console.log("Data products", data);
+  const data_seo = {
+    title: "Home Page",
+    keyword: "My keyword",
+    description: "Lorem Ipsum chỉ đơn giản là một đoạn văn bản giả, được dùng vào việc trình bày và dàn trang phục vụ cho in ấn"
+  };
   return (
     <>
+      <SEO seo={data_seo} />
       <Slider />
       <div className="wrap-main">
         <About about={data.about} />
